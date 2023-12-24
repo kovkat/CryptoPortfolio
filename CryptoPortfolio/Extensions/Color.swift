61e0ae05609aaa508aba7700dc7419841a0f648d
yarmolya <107544797+yarmolya@users.noreply.github.com>
2023-12-24 18:24:58 +0100

    Update Color.swift

--- CryptoPortfolio/Extensions/Color.swift
+++ CryptoPortfolio/Extensions/Color.swift
@@ -1,1 +1,35 @@
+//
+//  Color.swift
+//  CryptoPortfolio
+//
+
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    /// Color setUp for app
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+    let LaunchBackground = Color("LaunchBackground")
+}
+
+struct ColorTheme2 {
+    
+    let accent = Color.white
+    let background = Color.black
+    let green = Color.green
+    let red = Color.red
+    let secondaryText = Color.secondary
+    let LaunchBackground = Color.black
+}
 
