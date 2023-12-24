48a24811ede8f7362cbb56b04badac8e476fd7b0
yarmolya <107544797+yarmolya@users.noreply.github.com>
2023-12-24 18:35:50 +0100

    Update DismissButton.swift

--- CryptoPortfolio/Core/Components/DismissButton/DismissButton.swift
+++ CryptoPortfolio/Core/Components/DismissButton/DismissButton.swift
@@ -1,1 +1,34 @@
+//
+//  XMarkButton.swift
+//  CryptoPortfolio
+//
+
+
+
+import SwiftUI
+
+struct DismissButton: View {
+    
+    @Environment(\.dismiss) var goBack
+    
+    var body: some View {
+        Button {
+            hideKeyboard()
+            goBack()
+        } label: {
+            Image(systemName: "arrowshape.turn.up.backward.2")
+                .accessibilityIdentifier("dismiss_Button_ID")
+                .font(.headline)
+                .foregroundColor(Color.theme.red)
+        }
+        .padding(10)
+    }
+}
+
+//                🔱
+struct XMarkButton_Previews: PreviewProvider {
+    static var previews: some View {
+        DismissButton()
+    }
+}
 
