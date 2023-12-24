3e8682340a225afe6bf6f11001f6b052847f44b6
yarmolya <107544797+yarmolya@users.noreply.github.com>
2023-12-24 18:29:12 +0100

    Update CoinDataServiceProtocol.swift

--- CryptoPortfolio/Protocols/CoinDataServiceProtocol.swift
+++ CryptoPortfolio/Protocols/CoinDataServiceProtocol.swift
@@ -1,1 +1,19 @@
+//
+//  CoinDataServiceProtocol.swift
+//  CryptoPortfolio
+//
+
+
+
+import Foundation
+import Combine
+
+protocol CoinDataServiceProtocol {
+    
+    var publisher: Published<[CoinModel]>.Publisher { get }
+    
+    var coinSubscription: AnyCancellable?           { get }
+    
+    func getCoins()
+}
 
