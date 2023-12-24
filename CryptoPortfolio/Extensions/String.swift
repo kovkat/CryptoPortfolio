5933e9938086a1ae0023ce9d37b303772f655a16
yarmolya <107544797+yarmolya@users.noreply.github.com>
2023-12-24 18:26:12 +0100

    Update String.swift

--- CryptoPortfolio/Extensions/String.swift
+++ CryptoPortfolio/Extensions/String.swift
@@ -1,1 +1,17 @@
+//
+//  String.swift
+//  CryptoPortfolio
+//
+
+
+
+import Foundation
+
+extension String {
+    
+    var removingHTMLOccurrences: String {
+        /// Replace HTML code to ---> Void
+        return self.replacingOccurrences(of: "<[^>]+>", with: "" ,options: .regularExpression)
+    }
+}
 
