0408822bcea290324fc1a190e928744d2897fc7c
kovkat <kovalenkokateryna25@gmail.com>
2023-12-25 23:25:12 +0200

    Update NetworkingManager.swift
    net_manager

--- CryptoPortfolio/Utilities/NetworkingManager.swift
+++ CryptoPortfolio/Utilities/NetworkingManager.swift
@@ -1,1 +1,45 @@
 
+
+import Foundation
+import Combine
+
+class NetworkingManager {
+    
+    enum NetworkingError: LocalizedError, Equatable {
+        
+        case badURLResponse(url: URL)
+        case unowned
+        
+        var errorDescription: String? {
+            switch self {
+            case .badURLResponse(url: let url): return "[🔥] Bad response from URL: \(url)"
+            case .unowned                     : return "[⚠️] Unowned error"
+            }
+        }
+    }
+    
+    static func download(url: URL) -> AnyPublisher<Data, Error> {
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap({ try handleUrlResponse(output: $0, url: url )})
+            .retry(3)
+            .eraseToAnyPublisher()
+    }
+    
+    static func handleUrlResponse(output:  URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300 else {
+            throw NetworkingError.badURLResponse(url: url)
+        }
+        return output.data
+    }
+    
+    static func handleCompletion(_ completion: Subscribers.Completion<Error>) {
+        switch completion {
+        case .finished:
+            break
+        case .failure(let error):
+            print("[🔥] Error handleCompletion: \(error.localizedDescription)")
+        }
+    }
+}
