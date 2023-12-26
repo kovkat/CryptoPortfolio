b01a81acb4d3127f6a8af106b4e17b40c67becbf
kovkat <kovalenkokateryna25@gmail.com>
2023-12-26 09:11:32 +0200

    Update CoinImageView.swift

--- CryptoPortfolio/Core/Components/CoinImageView/CoinImageView.swift
+++ CryptoPortfolio/Core/Components/CoinImageView/CoinImageView.swift
@@ -1,1 +1,35 @@
 
+import SwiftUI
+
+struct CoinImageView: View {
+    
+    @StateObject private var vm: CoinImageViewModel
+    
+    init(coin: CoinModel) {
+        _vm = StateObject(wrappedValue: CoinImageViewModel(coin: coin))
+    }
+    
+    var body: some View {
+        ZStack {
+            if let image = vm.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            } else if vm.isLoading {
+                ProgressView()
+            } else {
+                Image(systemName: "questionmark.circle.fill")
+                    .foregroundColor(Color.theme.secondaryText)
+                    .font(.title)
+            }
+        }
+    }
+}
+
+struct CoinImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinImageView(coin: dev.coin)
+            .padding()
+            .previewLayout(.sizeThatFits)
+    }
+}
