31ced47e079be324eff3817544d7a5ff3ba6da7a
kovkat <kovalenkokateryna25@gmail.com>
2023-12-26 09:15:27 +0200

    Update CoinImageViewModel.swift

--- CryptoPortfolio/Core/Components/CoinImageView/CoinImageViewModel.swift
+++ CryptoPortfolio/Core/Components/CoinImageView/CoinImageViewModel.swift
@@ -1,1 +1,29 @@
 
+import SwiftUI
+import Combine
+
+class CoinImageViewModel: ObservableObject {
+    
+    @Published var image: UIImage? = nil
+    @Published var isLoading: Bool = false
+    
+    private let coin: CoinModel
+    private let dataService: CoinImageService
+    private var cancellable = Set<AnyCancellable>()
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        self.dataService = CoinImageService(coin: coin)
+        self.isLoading = true
+        self.addSubscribers()
+    }
+    
+    private func addSubscribers() {
+        dataService.$image
+            .sink { [weak self] _ in
+                self?.isLoading = false
+            } receiveValue: { [weak self] returnImage in
+                self?.image = returnImage
+            }.store(in: &cancellable)
+    }
+}
