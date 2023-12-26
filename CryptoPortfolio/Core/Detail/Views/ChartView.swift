a4c3e6ef0f044dc5f2572f06f721059975e4ec3a
kovkat <kovalenkokateryna25@gmail.com>
2023-12-26 09:50:51 +0200

    Update ChartView.swift

--- CryptoPortfolio/Core/Detail/Views/ChartView.swift
+++ CryptoPortfolio/Core/Detail/Views/ChartView.swift
@@ -1,1 +1,122 @@
 
+
+import SwiftUI
+
+struct ChartView: View {
+    
+    private let data: [Double]
+    private let maxY: Double
+    private let minY: Double
+    private let lineColor: Color
+    private let startingDate: Date
+    private let endingDate: Date
+    @State private var animateLine: CGFloat = 0
+    
+    init(coin: CoinModel) {
+        self.data = coin.sparklineIn7D?.price ?? []
+        self.maxY = data.max() ?? 0
+        self.minY = data.min() ?? 0
+        
+        /// SetUp for color of line
+        let priceChange = (data.last ?? 0) - (data.first ?? 0)
+        lineColor = priceChange > 0 ? Color.theme.green : Color.theme.red
+        
+        /// SetUp for Date - formatter
+        endingDate = Date(coinGecoString: coin.lastUpdated ?? "")
+        /// startingDate = endingDate - 7 days ago
+        startingDate = endingDate.addingTimeInterval(-7*24*60*60)
+    }
+    
+    var body: some View {
+        VStack {
+            chartView
+                .frame(height: 200)
+                .background(chartBackground)
+                .overlay(chartPrices.padding(.horizontal, 6), alignment: .leading)
+            
+            chartTimeInterval
+            
+        }
+        .font(.callout)
+        .foregroundColor(Color.theme.secondaryText)
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                withAnimation(.linear(duration: 2.0)) {
+                    animateLine = 1.0
+                }
+            }
+        }
+    }
+}
+
+
+struct ChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartView(coin: dev.coin)
+    }
+}
+
+
+extension ChartView {
+    
+    private var chartView: some View {
+        GeometryReader { geo in
+            Path { path in
+                for index in data.indices {
+                    
+                    let xPosition = geo.size.width / CGFloat(data.count) * CGFloat(index + 1)
+                    
+                    let yAxis = maxY - minY
+                    
+                    let yPosition = (1 - CGFloat((data[index] - minY) / yAxis)) * geo.size.height
+                    
+                    if index == 0 {
+                        path.move(to: CGPoint(x: xPosition, y: yPosition))
+                    }
+                    path.addLine(to: CGPoint(x: xPosition, y: yPosition))
+                }
+            }
+            .trim(from: 0, to: animateLine)
+            .stroke(lineColor, style: StrokeStyle(lineWidth: 3, lineCap: .round, lineJoin: .round))
+            .shadow(color: lineColor, radius: 10, x: 0.0, y: 10)
+            .shadow(color: lineColor.opacity(0.5), radius: 10, x: 0.0, y: 20)
+            .shadow(color: lineColor.opacity(0.3), radius: 10, x: 0.0, y: 30)
+            .shadow(color: lineColor.opacity(0.1), radius: 10, x: 0.0, y: 40)
+        }
+    }
+    
+    private var chartBackground: some View {
+        VStack {
+            Divider()
+            Spacer()
+            Divider()
+            Spacer()
+            Divider()
+        }
+    }
+    
+    private var chartPrices: some View {
+        VStack {
+            Text(maxY.formattedWithAbbreviations())
+            Spacer()
+            Text(((maxY + minY) / 2).formattedWithAbbreviations() )
+            Spacer()
+            Text(minY.formattedWithAbbreviations())
+        }
+        
+    }
+    
+    private var chartTimeInterval: some View {
+        HStack {
+            Text(startingDate.asShortDateString())
+            Spacer()
+            Text("7 day chart")
+                .accessibilityIdentifier("chart7Days_Label_ID")
+            Spacer()
+            Text(endingDate.asShortDateString())
+        }
+        .font(.callout)
+        .foregroundColor(Color.theme.secondaryText)
+        .padding(.horizontal, 6)
+    }
+}
